30d15bdde6637a9485d491a3755c2b60a5e2adf1
Felix Buehler <account@buehler.rocks>
2021-03-10 13:20:19 +0100

    [raspberry] disable network leds

--- hardware/raspberrypi4.nix
+++ hardware/raspberrypi4.nix
@@ -4,16 +4,21 @@
   boot.loader.grub.enable = false;
   boot.loader.raspberryPi.enable = true;
   boot.loader.raspberryPi.version = 4;
-  boot.loader.raspberryPi.firmwareConfig = "
+  boot.loader.raspberryPi.firmwareConfig = ''
     # Disable the ACT LED.
     dtparam=act_led_trigger=none
     dtparam=act_led_activelow=off
     # Disable the PWR LED.
     dtparam=pwr_led_trigger=none
     dtparam=pwr_led_activelow=off
+
+    # Disable ethernet port LEDs
+    dtparam=eth0_led=4
+    dtparam=eth1_led=4
     
     # Disable SD-Card pools
-    dtparam=sd_pool_once=on";
+    dtparam=sd_pool_once=on
+  '';
 
   # Kernel configuration
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
