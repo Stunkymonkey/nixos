056c0ab307a06d6da7abad94b9e11225f8f74611
Felix Buehler <account@buehler.rocks>
2022-01-08 16:35:54 +0100

    extra:bluetooth-audio: add sony-headphones-client

--- extra/bluetooth-audio.nix
+++ extra/bluetooth-audio.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
-
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
 {
   hardware.bluetooth = {
     enable = true;
@@ -11,4 +13,7 @@
     };
   };
   services.blueman.enable = true;
+  environment.systemPackages = with pkgs; [
+    unstable.sony-headphones-client
+  ];
 }
