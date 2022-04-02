a48c2321c9a24f4b1c5f8bdc6d2ee2ee8e33c708
Felix Buehler <account@buehler.rocks>
2022-04-02 15:30:18 +0200

    extra/nautilus: enable video thumbnails

--- extra/nautilus.nix
+++ extra/nautilus.nix
@@ -14,9 +14,16 @@ in
   services.gnome.glib-networking.enable = true; # network-mount
 
   environment.systemPackages = with pkgs; [
+    # thumbnails
+    ffmpegthumbnailer
     gnome.nautilus
+    # enable plugins
     gnome.nautilus-python
+    # thumbnails
+    gst_all_1.gst-libav
+    # default-programms
     shared-mime-info
+    # terminal-context-entry
     unstable.nautilus-open-any-terminal
   ];
 }
