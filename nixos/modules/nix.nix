24b1041da6be8734e5784c430ae82f136b945a07
Felix Buehler <account@buehler.rocks>
2022-06-03 18:00:07 +0200

    nix: enable standart flake command

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 {
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
   environment.systemPackages = with pkgs; [
     nix-prefetch
     nix-update
