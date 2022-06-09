3bc0e3f3b7a951b04c1087e71e3f44757d382c6b
Felix Buehler <account@buehler.rocks>
2022-06-09 23:20:39 +0200

    ssh: if not declared otherwise disable password-login

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -1,6 +1,9 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = lib.mkDefault false;
+  };
 
   # WARNING: if you remove this, then you need to assign a password to your user, otherwise
   # `sudo` won't work. You can do that either by using `passwd` after the first rebuild or
