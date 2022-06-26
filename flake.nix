48d6936889447a4d6d40dd63806d90c112e7cba5
Felix Buehler <account@buehler.rocks>
2022-06-26 22:40:59 +0200

    replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "NixOS configuration";
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
 
     nix.url = "github:NixOS/nix";
     nixpkgs.url = "nixpkgs/nixos-22.05";
@@ -18,5 +19,23 @@
       flake = false;
     };
   };
-  outputs = { ... } @ args: import ./outputs.nix args;
+  outputs = { self, flake-parts, deploy-rs, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit self; } {
+        imports = [
+          ./nixos/configurations.nix
+          #./nixos/images/default.nix
+          ./shell.nix
+        ];
+        systems = [ "x86_64-linux" "aarch64-linux" ];
+        perSystem = {inputs', ...}: {
+          # make pkgs available to all `perSystem` functions
+          _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
+        };
+        flake = {
+          checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+          deploy = import ./nixos/deploy.nix (inputs // {
+            inherit inputs;
+          });
+        };
+      };
 }
--- shell.nix
+++ shell.nix
@@ -1,17 +1,31 @@
-{ mkShellNoCC
-, ssh-to-age
-, sops
-, sops-import-keys-hook
-, deploy-rs
-, nixpkgs-fmt
-}:
+#{ mkShellNoCC
+#, ssh-to-age
+#, sops
+#, sops-import-keys-hook
+#, deploy-rs
+#, nixpkgs-fmt
+#}:
+#
+#mkShellNoCC {
+#  nativeBuildInputs = [
+#    ssh-to-age
+#    sops
+#    sops-import-keys-hook
+#    deploy-rs
+#    nixpkgs-fmt
+#  ];
+#}
 
-mkShellNoCC {
-  nativeBuildInputs = [
-    ssh-to-age
-    sops
-    sops-import-keys-hook
-    deploy-rs
-    nixpkgs-fmt
-  ];
+{
+  perSystem = { inputs', pkgs, ... }: {
+    # Definitions like this are entirely equivalent to the ones
+    # you may have directly in flake.nix.
+    devShells.default = pkgs.mkShellNoCC {
+      nativeBuildInputs = [
+        inputs'.sops-nix.packages.sops-import-keys-hook
+        inputs'.deploy-rs.packages.deploy-rs
+        pkgs.nixpkgs-fmt
+      ];
+    };
+  };
 }
