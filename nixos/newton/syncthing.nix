117da50fa3ee2835aaf08e770b5a4ded21e31002
Felix Buehler <account@buehler.rocks>
2022-07-03 10:51:43 +0200

    newton: add birdman to syncthing

--- nixos/newton/syncthing.nix
+++ nixos/newton/syncthing.nix
@@ -18,11 +18,25 @@
       "thinkman" = {
         id = "KXSCPX3-JCCFZM4-S2LQZZL-3AM6WRL-IPNWVG2-IB5FEDJ-YYFUIRR-VMDO3AL";
       };
+      "birdman" = {
+        id = "34Z4J7W-MJIODUD-J6LDJY6-QILQLLB-CJ4GR7K-7TJM2K3-R7SIPRV-XQO5TAI";
+      };
     };
     folders = {
       "Music" = {
-        path = "music";
-        devices = [ "thinkman" ];
+        id = "mphdq-n6q7y";
+        path = "/srv/data/music";
+        devices = [
+          "thinkman"
+          "birdman"
+        ];
+      };
+      "Pictures" = {
+        id = "cujyo-yiabu";
+        path = "/srv/data/photos";
+        devices = [
+          "thinkman"
+        ];
       };
     };
   };
