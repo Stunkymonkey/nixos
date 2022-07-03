f869cac21f20dcbe738cd75074a7cbb2c4d9e3f4
Felix Buehler <account@buehler.rocks>
2022-07-03 13:26:49 +0200

    newton: initial gitea

---
+++ nixos/modules/webapps/gitea.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+{
+  gitea = {
+    enable = true;
+    cookieSecure = true;
+    httpPort = 3001;
+    rootUrl = "https://git.buehler.rocks/";
+    log.level = "Warn";
+    disableRegistration = true;
+    settings = {
+      ui.DEFAULT_THEME = "arc-green";
+    };
+  };
+  webapps.apps.gitea = {
+    dashboard = {
+      name = "Git";
+      category = "app";
+      icon = "git";
+      link = "http://git.buehler.rocks";
+    };
+  };
+}
