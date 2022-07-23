0212bdf4f8d2d6f9ef57c072b0e3aeff74014763
Felix Buehler <account@buehler.rocks>
2022-07-23 19:08:32 +0200

    support autoupgrade with flakes

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -3,6 +3,12 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+  # support auto upgrade with flakes
+  system.autoUpgrade.flags = [
+    "--update-input"
+    "nixpkgs"
+    "--commit-lock-file"
+  ];
   environment.systemPackages = with pkgs; [
     nix-prefetch
     nix-update
