36c609932792f2fba8c2a99b4e73625432749d73
Felix Buehler <account@buehler.rocks>
2022-07-23 20:10:49 +0200

    enable mumble with correct certs

--- nixos/modules/service/mumble-server.nix
+++ nixos/modules/service/mumble-server.nix
@@ -3,7 +3,22 @@
   services.murmur = {
     enable = true;
     welcometext = "Welcome to the Mumble-Server!";
-    #sslKey = "";
-    #sslCert = "";
+    sslCert = "/var/lib/acme/voice.buehler.rocks/fullchain.pem";
+    sslKey = "/var/lib/acme/voice.buehler.rocks/key.pem";
   };
+
+  services.nginx.virtualHosts."voice.buehler.rocks".enableACME = true;
+  security.acme.certs."voice.buehler.rocks" = {
+    group = "voice-buehler-rocks";
+    postRun = ''
+      if ${pkgs.systemd}/bin/systemctl is-active murmur.service; then
+        ${pkgs.systemd}/bin/systemctl kill -s SIGUSR1 murmur.service
+      fi
+    '';
+  };
+
+  users.groups."voice-buehler-rocks".members = [ "murmur" "nginx" ];
+
+  networking.firewall.allowedTCPPorts = [ config.services.murmur.port ];
+  networking.firewall.allowedUDPPorts = [ config.services.murmur.port ];
 }
