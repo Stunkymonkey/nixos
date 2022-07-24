610967d8dcfeb2ac199d48614324bb36fc3fba0b
Felix Buehler <account@buehler.rocks>
2022-07-24 21:12:13 +0200

    serverle/hardware-config: remove nixos-hardware defaults

--- nixos/serverle/hardware-configuration.nix
+++ nixos/serverle/hardware-configuration.nix
@@ -1,9 +1,5 @@
 { config, pkgs, lib, ... }:
 {
-  # Boot
-  boot.loader.grub.enable = false;
-  boot.loader.raspberryPi.enable = true;
-  boot.loader.raspberryPi.version = 4;
   boot.loader.raspberryPi.firmwareConfig = ''
     # Disable the ACT LED.
     dtparam=act_led_trigger=none
@@ -21,13 +17,7 @@
   '';
 
   # Kernel configuration
-  boot.kernelPackages = pkgs.linuxPackages_rpi4;
   boot.kernelParams = [ "cma=64M" "console=tty0" ];
-
-  # Enable additional firmware (such as Wi-Fi drivers).
-  hardware.enableRedistributableFirmware = true;
-
-  #swapDevices = [{ device = "/swapfile"; size = 1024; }];
 
   # Fix wifi disconnect
   networking.networkmanager.wifi.powersave = false;
