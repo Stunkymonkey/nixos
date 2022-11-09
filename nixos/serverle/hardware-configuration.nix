a354d4c88e0a82c5f01ad905eb98da1ac9fe6046
Felix Buehler <account@buehler.rocks>
2022-11-09 22:21:41 +0100

    serverle: switch to FAT-bootloader

--- nixos/serverle/hardware-configuration.nix
+++ nixos/serverle/hardware-configuration.nix
@@ -1,20 +1,29 @@
 { config, pkgs, lib, ... }:
 {
-  boot.loader.raspberryPi.firmwareConfig = ''
-    # Disable the ACT LED.
-    dtparam=act_led_trigger=none
-    dtparam=act_led_activelow=off
-    # Disable the PWR LED.
-    dtparam=pwr_led_trigger=none
-    dtparam=pwr_led_activelow=off
+  boot.loader = {
+    raspberryPi = {
+      firmwareConfig = ''
+        # Disable the ACT LED.
+        dtparam=act_led_trigger=none
+        dtparam=act_led_activelow=off
+        # Disable the PWR LED.
+        dtparam=pwr_led_trigger=none
+        dtparam=pwr_led_activelow=off
 
-    # Disable ethernet port LEDs
-    dtparam=eth0_led=4
-    dtparam=eth1_led=4
+        # Disable ethernet port LEDs
+        dtparam=eth0_led=4
+        dtparam=eth1_led=4
     
-    # Disable SD-Card pools
-    dtparam=sd_pool_once=on
-  '';
+        # Disable SD-Card pools
+        dtparam=sd_pool_once=on
+      '';
+
+      # the bootloader has to be enabled for fat systems. for ext use the other one.
+      enable = true;
+      version = 4;
+    };
+    generic-extlinux-compatible.enable = false;
+  };
 
   # Kernel configuration
   boot.kernelParams = [ "cma=64M" "console=tty0" ];
