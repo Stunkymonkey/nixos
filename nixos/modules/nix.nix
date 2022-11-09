1f1fb9ca39359fe59b415d462a77c5f71c644999
Felix Buehler <account@buehler.rocks>
2022-11-09 22:28:10 +0100

    nix: trust wheel group and enable gc

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,14 +1,29 @@
 { config, lib, pkgs, ... }:
 {
+  nix.settings = {
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+    auto-optimise-store = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 30d";
+  };
+
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+
   # support auto upgrade with flakes
   system.autoUpgrade.flags = [
     "--update-input"
     "nixpkgs"
     "--commit-lock-file"
   ];
+
   environment.systemPackages = with pkgs; [
     nix-prefetch
     nix-update
