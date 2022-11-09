a1ffa50498acd3a1d3debff4c88760ff97d5e2f2
Felix Buehler <account@buehler.rocks>
2022-11-09 22:20:45 +0100

    homepage: add new input

---
+++ modules/services/homepage/default.nix
@@ -0,0 +1,30 @@
+# My own personal homepage
+{ config, lib, pkgs, inputs, ... }:
+let
+  cfg = config.my.services.homepage;
+  domain = config.networking.domain;
+in
+{
+  options.my.services.homepage = with lib; {
+    enable = mkEnableOption "Stunkymonkey-Hompage";
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    my.services.nginx.virtualHosts = [
+      {
+        subdomain = "blog";
+        root = inputs.stunkymonkey.packages.${config.nixpkgs.system}.default;
+      }
+    ];
+
+    webapps.apps.homepage = {
+      dashboard = {
+        name = "Homepage";
+        category = "other";
+        icon = "blog";
+        link = "https://blog.${domain}";
+      };
+    };
+  };
+}
