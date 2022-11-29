87070966a00eabe5586dafe656e75374a518c822
Felix Buehler <account@buehler.rocks>
2022-11-29 18:45:37 +0100

    serverle: start using my-modules

---
+++ nixos/serverle/services.nix
@@ -0,0 +1,13 @@
+# Deployed services
+{ config, lib, ... }:
+let
+  secrets = config.sops.secrets;
+in
+{
+  # List services that you want to enable:
+  my.services = {
+    ssh-server = {
+      enable = true;
+    };
+  };
+}
