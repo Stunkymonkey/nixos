a34e69564e2f0480dee789a2355b47a1cb2e5565
Felix Buehler <account@buehler.rocks>
2022-12-03 11:59:58 +0100

    serverle: fix wifi with NetworkManager

--- nixos/serverle/wifi.nix
+++ nixos/serverle/wifi.nix
@@ -1,11 +1,6 @@
 { config, ... }:
 {
-  sops.secrets."wifi/bismarck" = { };
-
-  networking.wireless = {
-    environmentFile = config.sops.secrets."wifi/bismarck".path;
-    networks = {
-      "Bismarck WLAN".psk = "@PSK_BISMARCK@";
-    };
+  sops.secrets."wifi/bismarck" = {
+    path = "/etc/NetworkManager/system-connections/Bismarck WLAN.nmconnection";
   };
 }
