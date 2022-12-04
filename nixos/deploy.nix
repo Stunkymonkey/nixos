e7b6c789dc9c18385d8c5ab8b2d91f4dbc805e2b
Felix Buehler <account@buehler.rocks>
2022-12-04 18:08:58 +0100

    deploy-rs: fix config

--- nixos/deploy.nix
+++ nixos/deploy.nix
@@ -1,22 +1,23 @@
-{ self
-, deploy-rs
-, ...
-}:
+{ self, ... }:
 let
-  mkNode = server: ip: fast: {
-    hostname = "${ip}:22";
-    fastConnection = fast;
+  inherit (self.inputs) deploy-rs;
+  mkNode = server: hostname: system: remoteBuild: {
+    inherit hostname remoteBuild;
+    fastConnection = true;
     profiles.system.path =
-      deploy-rs.lib.x86_64-linux.activate.nixos
+      deploy-rs.lib.${system}.activate.nixos
         self.nixosConfigurations."${server}";
   };
 in
 {
   user = "root";
-  #sshUser = "felix";
-  sshUser = "root";
+  sshUser = "felix";
+  sshOpts = [ "-i" "~/.ssh/keys/local_ed25519" ];
+  #sshOpts = [ "-p" "6158" "-i" "~/.ssh/keys/local_ed25519" ];
+
   nodes = {
-    serverle = mkNode "serverle" "serverle.local" true;
-    newton = mkNode "newton" "buehler.rocks" true;
+    thinkman = mkNode "thinkman" "localhost" "x86_64-linux" false;
+    newton = mkNode "newton" "buehler.rocks" "x86_64-linux" false;
+    serverle = mkNode "serverle" "serverle.local" "aarch64-linux" true;
   };
 }
