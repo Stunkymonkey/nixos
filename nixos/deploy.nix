1ca1981457c582b554527e00e7ced814e167709f
Felix Buehler <account@buehler.rocks>
2022-12-05 20:17:39 +0100

    deploy-rs: enable remoteBuilding for all hosts

--- nixos/deploy.nix
+++ nixos/deploy.nix
@@ -1,8 +1,8 @@
 { self, ... }:
 let
   inherit (self.inputs) deploy-rs;
-  mkNode = server: hostname: system: remoteBuild: {
-    inherit hostname remoteBuild;
+  mkNode = server: hostname: system: {
+    inherit hostname;
     fastConnection = true;
     profiles.system.path =
       deploy-rs.lib.${system}.activate.nixos
@@ -14,10 +14,11 @@ in
   sshUser = "felix";
   sshOpts = [ "-i" "~/.ssh/keys/local_ed25519" ];
   #sshOpts = [ "-p" "6158" "-i" "~/.ssh/keys/local_ed25519" ];
+  remoteBuild = true;
 
   nodes = {
-    thinkman = mkNode "thinkman" "localhost" "x86_64-linux" false;
-    newton = mkNode "newton" "buehler.rocks" "x86_64-linux" false;
-    serverle = mkNode "serverle" "serverle.local" "aarch64-linux" true;
+    thinkman = mkNode "thinkman" "localhost" "x86_64-linux";
+    newton = mkNode "newton" "buehler.rocks" "x86_64-linux";
+    serverle = mkNode "serverle" "serverle.local" "aarch64-linux";
   };
 }
