91b8d0eb016f4a495e46319a4575801000029205
Felix Buehler <account@buehler.rocks>
2022-12-05 22:54:25 +0100

    core/users: add podman and rearange

--- machines/core/users.nix
+++ machines/core/users.nix
@@ -8,15 +8,16 @@
     home = "/home/felix";
     group = "felix";
     extraGroups = [
-      "wheel"
-      "adbusers"
-      "audio"
-      "dialout"
-      "docker"
-      "input"
-      "libvirtd"
-      "networkmanager"
-      "video"
+      "adbusers" # adb control
+      "audio" # sound control
+      "dialout" # serial-console
+      "docker" # usage of `docker` socket
+      "input" # mouse control
+      "libvirtd" # kvm control
+      "networkmanager" # wireless configuration
+      "podman" # usage of `podman` socket
+      "video" # screen control
+      "wheel" # `sudo` for the user.
     ];
     passwordFile = config.sops.secrets."users/felix/password".path;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFx6OLwL9MbkD3mnMsv+xrzZHN/rwCTgVs758SCLG0h felix@thinkman" ];
