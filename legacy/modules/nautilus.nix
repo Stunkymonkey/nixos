f80f0f2c33f823222de31e41360432e505418c34
Felix Buehler <account@buehler.rocks>
2023-01-16 22:51:02 +0100

    legacy/nautilus: update to gnome 43

--- legacy/modules/nautilus.nix
+++ legacy/modules/nautilus.nix
@@ -3,12 +3,16 @@
   # enable trash & network-mount
   services.gvfs.enable = true;
 
-  environment.sessionVariables.NAUTILUS_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-3.0";
+  environment.sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
   environment.pathsToLink = [
     "/share/nautilus-python/extensions"
   ];
 
   services.gnome.glib-networking.enable = true; # network-mount
+
+  # default-programms
+  xdg.mime.enable = true;
+  xdg.icons.enable = true;
 
   environment.systemPackages = with pkgs; [
     # thumbnails
@@ -18,8 +22,6 @@
     gnome.nautilus-python
     # thumbnails
     gst_all_1.gst-libav
-    # default-programms
-    shared-mime-info
     # terminal-context-entry
     nautilus-open-any-terminal
   ];
