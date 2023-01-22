9f3ecc597254ac46abcb887f30ea9c29bde31ea6
Felix Buehler <account@buehler.rocks>
2023-01-22 17:26:39 +0100

    legacy/power: enable powertop again

--- legacy/modules/power.nix
+++ legacy/modules/power.nix
@@ -1,15 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    config.boot.kernelPackages.cpupower
-    pkgs.powertop
-    pkgs.s-tui
+  environment.systemPackages = with pkgs; [
+    powertop
+    s-tui
   ];
 
   powerManagement = {
-    cpuFreqGovernor = lib.mkDefault "powersave";
-    #powertop.enable = true; # messes up usb mouse
+    cpuFreqGovernor = "powersave";
+    powertop.enable = true;
   };
 
   services = {
