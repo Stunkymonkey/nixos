3add8feef4183ada4d948fe32c638a57fb144ea0
Felix Buehler <account@buehler.rocks>
2023-03-13 22:22:03 +0100

    freshrss: update package

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,13 @@
+# overlays for nixpkgs
 self: super:
 {
-  # no overlays yet
+  # freshrss = super.freshrss.overrideAttrs (old: {
+  #   version = "1.21.0";
+  #   src = super.fetchFromGitHub {
+  #     owner = "FreshRSS";
+  #     repo = "FreshRSS";
+  #     rev = "1.21.0";
+  #     hash = "sha256-0+fMZ5ps0CkBbS+fcxlYrrkQi28tmrKTyl3kPuofqyI=";
+  #   };
+  # });
 }
