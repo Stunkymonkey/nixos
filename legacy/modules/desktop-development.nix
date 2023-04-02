ba05b45982fe48b363aec81b0e535e7d5c444452
Felix Buehler <account@buehler.rocks>
2023-04-02 12:16:55 +0200

    legacy/desktop-dev: customize vscodium

--- legacy/modules/desktop-development.nix
+++ legacy/modules/desktop-development.nix
@@ -13,6 +13,31 @@
     unstable.qgis
     sqlitebrowser
     unstable.sublime4
-    vscodium
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions =
+        with vscode-extensions; [
+          bbenoist.nix
+          coenraads.bracket-pair-colorizer-2
+          editorconfig.editorconfig
+          mkhl.direnv
+          ms-azuretools.vscode-docker
+          ms-python.python
+          ms-vscode-remote.remote-ssh
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "remote-ssh-edit";
+            publisher = "ms-vscode-remote";
+            version = "0.47.2";
+            sha256 = "sha256-LxFOxkcQNCLotgZe2GKc2aGWeP9Ny1BpD1XcTqB85sI=";
+          }
+          {
+            name = "vsc-material-theme";
+            publisher = "Equinusocio";
+            version = "33.8.0";
+            sha256 = "sha256-+I4AUwsrElT62XNvmuAC2iBfHfjNYY0bmAqzQvfwUYM=";
+          }
+        ];
+    })
   ];
 }
