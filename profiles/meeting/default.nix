50bfbb571f6f58dd5699906e1710a5a3b7273f49
Felix Buehler <account@buehler.rocks>
2023-08-17 22:16:40 +0200

    profile/meeting: fix elements-desktop

--- profiles/meeting/default.nix
+++ profiles/meeting/default.nix
@@ -9,7 +9,8 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      element-desktop
+      # TODO revert fix: https://github.com/NixOS/nixpkgs/issues/238416
+      (element-desktop.override { electron = pkgs.electron_24; })
       mumble
       nheko
       teamspeak_client
