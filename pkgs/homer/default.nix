589505a9d8964a0429d1481614ab52e5cb815380
Felix Buehler <account@buehler.rocks>
2023-10-21 16:22:32 +0200

    pkgs/homer: 23.9.1 -> 23.10.1

--- pkgs/homer/default.nix
+++ pkgs/homer/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchzip }:
 stdenv.mkDerivation rec {
   pname = "homer";
-  version = "23.09.1";
+  version = "23.10.1";
 
   src = fetchzip {
     url = "https://github.com/bastienwirtz/homer/releases/download/v${version}/homer.zip";
-    hash = "sha256-l6bfNzbW2uNaAVdBztG6B8dFnrRjQGBUCp9g+VoS1II=";
+    hash = "sha256-KUEqrjO9LAoigZsQGLy5JrtsXx+HDXaz4Y4Vpba0uNw=";
     stripRoot = false;
   };
 
