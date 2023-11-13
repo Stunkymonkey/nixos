9d6d14c393d64a895f931bccd1d8f9cb7c3e58e7
Felix Buehler <account@buehler.rocks>
2023-11-13 20:36:31 +0100

    machines{serverle,newton}: fix syncthing for 23.11

--- machines/newton/syncthing.nix
+++ machines/newton/syncthing.nix
@@ -17,60 +17,60 @@
     openDefaultPorts = true;
     key = config.sops.secrets."syncthing/key".path;
     cert = config.sops.secrets."syncthing/cert".path;
-    extraOptions = {
+    settings = {
       options = {
         localAnnounceEnabled = false;
         urAccepted = 3;
       };
-    };
-    devices = {
-      "thinkman" = {
-        id = "KXSCPX3-JCCFZM4-S2LQZZL-3AM6WRL-IPNWVG2-IB5FEDJ-YYFUIRR-VMDO3AL";
+      devices = {
+        "thinkman" = {
+          id = "KXSCPX3-JCCFZM4-S2LQZZL-3AM6WRL-IPNWVG2-IB5FEDJ-YYFUIRR-VMDO3AL";
+        };
+        "birdman" = {
+          id = "34Z4J7W-MJIODUD-J6LDJY6-QILQLLB-CJ4GR7K-7TJM2K3-R7SIPRV-XQO5TAI";
+        };
+        "serverle" = {
+          id = "PVPEIN7-PI226LR-ULSBYKT-JGRQ3PS-WSPLGBP-TKYRJVP-OTWE7IV-NLKTBA3";
+        };
       };
-      "birdman" = {
-        id = "34Z4J7W-MJIODUD-J6LDJY6-QILQLLB-CJ4GR7K-7TJM2K3-R7SIPRV-XQO5TAI";
-      };
-      "serverle" = {
-        id = "PVPEIN7-PI226LR-ULSBYKT-JGRQ3PS-WSPLGBP-TKYRJVP-OTWE7IV-NLKTBA3";
-      };
-    };
-    folders = {
-      "Computer" = {
-        id = "djdxo-1akub";
-        path = "/data/computer";
-        devices = [
-          "thinkman"
-          "birdman"
-          "serverle"
-        ];
-      };
-      "Phone" = {
-        id = "4hds7-gpypp";
-        path = "/data/phone";
-        devices = [
-          "thinkman"
-          "birdman"
-          "serverle"
-        ];
-      };
-      "Music" = {
-        id = "mphdq-n6q7y";
-        path = "/data/music";
-        watch = false;
-        devices = [
-          "thinkman"
-          "birdman"
-          "serverle"
-        ];
-      };
-      "Pictures" = {
-        id = "cujyo-yiabu";
-        path = "/data/photos";
-        watch = false;
-        devices = [
-          "thinkman"
-          "serverle"
-        ];
+      folders = {
+        "Computer" = {
+          id = "djdxo-1akub";
+          path = "/data/computer";
+          devices = [
+            "thinkman"
+            "birdman"
+            "serverle"
+          ];
+        };
+        "Phone" = {
+          id = "4hds7-gpypp";
+          path = "/data/phone";
+          devices = [
+            "thinkman"
+            "birdman"
+            "serverle"
+          ];
+        };
+        "Music" = {
+          id = "mphdq-n6q7y";
+          path = "/data/music";
+          fsWatcherEnabled = false;
+          devices = [
+            "thinkman"
+            "birdman"
+            "serverle"
+          ];
+        };
+        "Pictures" = {
+          id = "cujyo-yiabu";
+          path = "/data/photos";
+          fsWatcherEnabled = false;
+          devices = [
+            "thinkman"
+            "serverle"
+          ];
+        };
       };
     };
   };
