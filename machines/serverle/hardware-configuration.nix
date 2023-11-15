7ea77d2da069399d037fb36feef471533b2a9e29
Felix Buehler <account@buehler.rocks>
2023-11-15 21:09:28 +0100

    machine/serverle: migrate from propierietary to u-boot bootloader

--- machines/serverle/hardware-configuration.nix
+++ machines/serverle/hardware-configuration.nix
@@ -1,32 +1,14 @@
-_:
+{ pkgs, ... }:
 {
-  boot = {
-    loader = {
-      raspberryPi = {
-        firmwareConfig = ''
-          # Disable the ACT LED.
-          dtparam=act_led_trigger=none
-          dtparam=act_led_activelow=off
-          # Disable the PWR LED.
-          # somehow this changed in firmware settings: https://github.com/raspberrypi/firmware/issues/1742
-          #dtparam=pwr_led_trigger=default-on
-          dtparam=pwr_led_activelow=off
-
-          # Disable ethernet port LEDs
-          dtparam=eth0_led=4
-          dtparam=eth1_led=4
-
-          # Disable SD-Card pools
-          dtparam=sd_pool_once=on
-        '';
-
-        # the bootloader has to be enabled for fat systems. for ext use the other one.
-        enable = true;
-        version = 4;
-      };
-      generic-extlinux-compatible.enable = false;
+  hardware = {
+    deviceTree = {
+      enable = true;
+      filter = "*rpi-4-*.dtb";
     };
-    # Kernel configuration
-    kernelParams = [ "cma=64M" "console=tty0" ];
   };
+
+  environment.systemPackages = with pkgs; [
+    libraspberrypi
+    raspberrypi-eeprom
+  ];
 }
