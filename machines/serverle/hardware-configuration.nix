05f2c244b6eb72af795af4fd397e823e6a2bda0b
Felix Buehler <account@buehler.rocks>
2023-11-19 18:26:20 +0100

    machine/serverle: disable leds

--- machines/serverle/hardware-configuration.nix
+++ machines/serverle/hardware-configuration.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 {
   hardware = {
+    raspberry-pi."4".leds = {
+      eth.disable = true;
+      act.disable = true;
+      pwr.disable = true;
+    };
     deviceTree = {
       enable = true;
       filter = "*rpi-4-*.dtb";
