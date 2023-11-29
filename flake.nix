12c7c9111ebf16f9c255865078161d042e84dcc5
Felix Buehler <account@buehler.rocks>
2023-11-29 22:38:52 +0100

    flake: migrate to nixos 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
   inputs = {
     nix.url = "github:NixOS/nix";
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
