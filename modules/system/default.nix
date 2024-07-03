387eed714d3a418c78c37cd7f6151edbb74d15c1
Felix Buehler <account@buehler.rocks>
2024-07-03 22:29:37 +0200

    system/spell-check: init

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -6,5 +6,6 @@
     ./fonts
     ./kvm
     ./podman
+    ./spell-check
   ];
 }
---
+++ modules/system/spell-check/default.nix
@@ -0,0 +1,19 @@
+# spell-checking
+{ config, lib, options, pkgs, ... }:
+let
+  cfg = config.my.system.spell-check;
+in
+{
+  options.my.system.spell-check = with lib; {
+    enable = mkEnableOption "spell-check configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      aspell
+      aspellDicts.en
+      aspellDicts.en-computers
+      aspellDicts.en-science
+    ];
+  };
+}
