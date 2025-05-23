9c64b82610a46e385dd044f1b71da02c818943da
Felix Buehler <account@buehler.rocks>
2025-05-23 22:59:11 +0200

    service/nextcloud: 30 -> 31

--- modules/services/nextcloud/default.nix
+++ modules/services/nextcloud/default.nix
@@ -53,7 +53,7 @@ in
     services = {
       nextcloud = {
         enable = true;
-        package = pkgs.nextcloud30;
+        package = pkgs.nextcloud31;
         hostName = "cloud.${domain}";
         maxUploadSize = cfg.maxSize;
         autoUpdateApps.enable = true;
@@ -65,6 +65,7 @@ in
           adminuser = cfg.admin;
           adminpassFile = cfg.passwordFile;
 
+          dbtype = "sqlite";
           #dbtype = "pgsql";
           #dbhost = "/run/postgresql";
         };
