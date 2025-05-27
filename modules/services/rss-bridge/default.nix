efd88fb11bb26af4dabbfd42bb6d883fd22be99a
Felix Buehler <account@buehler.rocks>
2025-05-28 00:15:20 +0200

    service/rss-bridge: use official caddy setting

--- modules/services/rss-bridge/default.nix
+++ modules/services/rss-bridge/default.nix
@@ -18,24 +18,9 @@ in
     services.rss-bridge = {
       enable = true;
       config.system.enabled_bridges = [ "*" ]; # Whitelist all
-      virtualHost = null;
-      user = "caddy";
-      group = "caddy";
-      # TODO: with 25.05 this can be simplified via
-      # webserver = "caddy";
+      virtualHost = domain;
+      webserver = "caddy";
     };
-    my.services.webserver.virtualHosts = [
-      {
-        subdomain = "rss-bridge";
-        extraConfig = ''
-          root * ${pkgs.rss-bridge}
-          php_fastcgi unix/${config.services.phpfpm.pools."rss-bridge".socket} {
-            env RSSBRIDGE_fileCache_path ${config.services.rss-bridge.dataDir}/cache/
-          }
-          file_server
-        '';
-      }
-    ];
 
     webapps.apps.rss-bridge = {
       dashboard = {
