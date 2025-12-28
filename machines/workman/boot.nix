e45ffe2fe3685a237e6208e834a13f6f2b84d016
Felix Buehler <account@buehler.rocks>
2025-12-28 15:35:08 +0100

    machine/workman: fix boot screen

--- machines/workman/boot.nix
+++ machines/workman/boot.nix
@@ -5,15 +5,28 @@
 {
   boot = {
     loader = {
-      timeout = 1;
+      timeout = 0;
       systemd-boot = {
         enable = true;
         configurationLimit = 10;
-        consoleMode = "keep";
+        consoleMode = "max"; # needed for plymouth
         editor = true;
       };
       efi.canTouchEfiVariables = true;
     };
+    initrd = {
+      systemd.enable = true; # for a nice password prompt
+      verbose = false;
+    };
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
     plymouth = {
       enable = true;
       theme = "framework";
