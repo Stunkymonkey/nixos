28857da0a57cd7e52a4a06cbd8917a12d1148f4d
Felix Buehler <account@buehler.rocks>
2025-12-29 12:32:11 +0100

    hardware/bluetooth: enable powerOnBoot

--- modules/hardware/bluetooth/default.nix
+++ modules/hardware/bluetooth/default.nix
@@ -15,7 +15,7 @@ in
   config = lib.mkIf cfg.enable {
     hardware.bluetooth = {
       enable = true;
-      package = pkgs.bluez;
+      powerOnBoot = true;
       settings = {
         General = {
           Enable = "Source,Sink,Media,Socket";
