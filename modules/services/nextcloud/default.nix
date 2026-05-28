ea895bb55129adf642738b79cf98f253e324024b
Felix Buehler <account@buehler.rocks>
2026-05-29 00:00:08 +0200

    service/nextcloud: 32 -> 33

--- modules/services/nextcloud/default.nix
+++ modules/services/nextcloud/default.nix
@@ -53,7 +53,7 @@ in
     services = {
       nextcloud = {
         enable = true;
-        package = pkgs.nextcloud32;
+        package = pkgs.nextcloud33;
         hostName = "cloud.${domain}";
         maxUploadSize = cfg.maxSize;
         autoUpdateApps.enable = true;
