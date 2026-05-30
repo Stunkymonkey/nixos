767d5032609b669de02721e3362ec570d36642d6
Felix Buehler <account@buehler.rocks>
2026-05-30 20:40:22 +0200

    nixos: 25.11 -> 26.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS configuration";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
