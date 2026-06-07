8e323d5d78c98f6751ca91f16ad9b7e5c8288aa4
Felix Buehler <account@buehler.rocks>
2026-06-07 20:11:03 +0200

    service/initrd-wifi: disable target for now
    have to enable this in the future again. somehow breaks the machines

--- modules/services/initrd-wifi/default.nix
+++ modules/services/initrd-wifi/default.nix
@@ -23,7 +23,7 @@ in
     configFile = lib.mkOption {
       type = lib.types.path;
       example = "/run/secret/wpa_supplicant.conf";
-      description = "wpa_supplicant.conf path containing ssid info. This should be encrypted in your repo";
+      description = "wpa_supplicant.conf path containing ssid info.";
     };
   };
   config = lib.mkIf cfg.enable {
@@ -42,12 +42,12 @@ in
       systemd = {
         packages = [ pkgs.wpa_supplicant ];
         initrdBin = [ pkgs.wpa_supplicant ];
-        targets.initrd.wants = [ "wpa_supplicant@${cfg.interface}.service" ];
+        # targets.initrd.wants = [ "wpa_supplicant@${cfg.interface}.service" ];
         services."wpa_supplicant@".unitConfig.DefaultDependencies = false;
 
         network = {
           enable = true;
-          networks."10-wlan" = {
+          networks."20-wlan" = {
             matchConfig.Name = cfg.interface;
             networkConfig.DHCP = "yes";
           };
